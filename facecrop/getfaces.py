5b98001c69c158489804ff28c10e153f0800ec94
Alex Choi <rlrrlrll4@gmail.com>
2015-11-07 04:45:38 -0500

    facecropping works

--- facecrop/getfaces.py
+++ facecrop/getfaces.py
@@ -1,4 +1,5 @@
-import urllib, urllib2, sys
+import urllib, urllib2, sys, calendar, time, json
+from PIL import Image
 
 def getRequestURL(imageUrl, apiKey):
 	retUrl = "https://api.havenondemand.com/1/api/sync/detectfaces/v1?url=" + urllib.quote(imageUrl) + "&additional=false&apikey=" + apiKey
@@ -8,11 +9,32 @@ def makeRequest(requestURL):
 	content = urllib2.urlopen(requestURL).read()
 	return content
 
-def cropImage(imageUrl):
+def downloadImage(imageUrl, imageName):
 	# download image
-	with open("sample.jpg", "r") as f:
-		f.write(urllib.urlopen(imageUrl))
+	urllib.urlretrieve(imageUrl, imageName)
+
+def cropImage(imageName, extension, count, left, top, width, height):
+	with Image.open(imageName + extension, "r") as img:
+		cropImage = img.crop((left, top, left + width, top + height))
+		cropImage.save(imageName + "_" + str(count).zfill(3) + extension)
+
+
 
 
 if __name__ == "__main__":
-	print(makeRequest(getRequestURL(sys.argv[1], sys.argv[2])))
+	imageUrl = sys.argv[1]
+	apiKey = sys.argv[2]
+
+	facesJson = json.loads(makeRequest(getRequestURL(imageUrl, apiKey)))
+	imageName = "cropped_faces/" + str(calendar.timegm(time.strptime('Jul 9, 2009 @ 20:02:58 UTC', '%b %d, %Y @ %H:%M:%S UTC')))
+	extension = imageUrl[imageUrl.rfind("."):]
+	downloadImage(imageUrl, imageName + extension)
+	
+	count = 0
+	for face in facesJson.get("face"):
+		left = face.get("left")
+		top = face.get("top")
+		width = face.get("width")
+		height = face.get("height")
+		cropImage(imageName, extension, count, left, top, width, height)
+		count += 1
